8fb4cc7d42ba61fd732e5e1775e3afebc952d70c
Ari <ariosym@gmail.com>
2023-10-23 16:37:53 -0400

    Added Timeline to list of routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import BrowserPage from './BrowserPage'
 import CanvasPage from '@/_components/CanvasPage'
+import Timeline from '@/Timeline/Timeline'
 
 const videoPath = "/Users/ari/Desktop/oClip/council_2023-03-23/VIDEO/NYCC-PV-250-14_230323-103916.mp4"
 
@@ -14,6 +15,8 @@ export default function App() {
         <Route path="/" element={<BrowserPage/>} />
         
         <Route path="/canvas" element={<CanvasPage videoPath={videoPath} />}  />
+
+        <Route path="timeline" element={<Timeline videoPath={videoPath} />} />
 
       </Routes>
 
