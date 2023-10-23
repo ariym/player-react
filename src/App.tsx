8111eac30c8235cef680d9277ff1d4b83443544f
Ari <ariosym@gmail.com>
2023-10-23 15:48:11 -0400

    started work on Timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import BrowserPage from './BrowserPage'
-import CanvasPage from './CanvasPage'
+import CanvasPage from './Experiments/CanvasPage'
 
 const videoPath = "/Users/ari/Desktop/oClip/council_2023-03-23/VIDEO/NYCC-PV-250-14_230323-103916.mp4"
 
