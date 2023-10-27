1742fb02b730e099bb3edcfd56ed8f9453147d9e
Ari <ariosym@gmail.com>
2023-10-26 20:21:57 -0400

    Removed extra lines

--- src/_components/Player.tsx
+++ src/_components/Player.tsx
@@ -10,8 +10,6 @@ export default function Player({ url, seekToTs }: { url: any, seekToTs: any }) {
     // file: {
     // tracks: [
     // { kind: 'subtitles', src: 'subs/subtitles.en.vtt', srcLang: 'en', default: true },
-    // { kind: 'subtitles', src: 'subs/subtitles.ja.vtt', srcLang: 'ja' },
-    // { kind: 'subtitles', src: 'subs/subtitles.de.vtt', srcLang: 'de' }
     // ]
     // }
   }
@@ -19,7 +17,6 @@ export default function Player({ url, seekToTs }: { url: any, seekToTs: any }) {
   useEffect(() => {
     ref.current.seekTo(seekToTs)
   }, [seekToTs]);
-
 
   return (
     <ReactPlayer
