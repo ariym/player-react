7842a5d5b3815c25d05b7d4ee20821ff1af321a0
Ari <ariosym@gmail.com>
2024-07-29 14:28:47 -0400

    remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import FilePlayerPage from './FilePlayerPage'
-import CanvasPage from '@/_components/CanvasPage'
-import QueryPlayer from '@/_components/QueryPlayer'
-import StreamFFmpeg from '@/StreamFFmpeg'
-import TimelinePage from '@/Timeline/Timeline'
-
-
-const DEFAULT_VIDEO_PATH = import.meta.env.VITE_DEFAULT_VIDEO_PATH;
-const DEFAULT_DIR_PATH = import.meta.env.VITE_DEFAULT_DIR_PATH;
-
+import HomePage from '@/HomePage'
+import FolderPage from '@/FolderPage'
+import UrlStreamPlayer from "@/_components/UrlStreamPlayer";
 
 let routes = {
-  "/": <StreamFFmpeg
-    defaultDirPath={DEFAULT_DIR_PATH}
-    defaultVideoPath={DEFAULT_VIDEO_PATH}
-  />,
-  "canvas": <CanvasPage videoPath={DEFAULT_VIDEO_PATH} />,
-  "query-player": <QueryPlayer videoPath={DEFAULT_VIDEO_PATH} />,
-  "file-player": <FilePlayerPage
-    // todo: REPLACE these props with a in-route call to react-query
-    defaultDirPath={DEFAULT_DIR_PATH}
-    defaultVideoPath={DEFAULT_VIDEO_PATH}
-  />,
+  "/": <HomePage />,
+  "/folder": <FolderPage />,
+  "/file-player": <UrlStreamPlayer />
 }
-
 
 export default function App() {
   return (
